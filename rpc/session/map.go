10805c04a11d672370e671b26a9af66c36d6e61a
Vasileios Bardakos <vasilisbardakos@gmail.com>
2026-01-06 09:12:16 +0200

    refactor: renames

--- rpc/session/map.go
+++ rpc/session/map.go
@@ -8,46 +8,46 @@ import (
 
 type mapManagerID struct{}
 
-type safemapManager[ID comparable, ST any] struct {
+type MapManager[ID comparable, ST any] struct {
 	ss    *i.SafeMap[ID, *ST]
 	newFn func(ID) *ST
 }
 
-func NewGenMapManager[ID comparable, ST any](fn func(ID) *ST) *safemapManager[ID, ST] {
-	return &safemapManager[ID, ST]{ss: i.NewMap[ID, *ST](), newFn: fn}
+func NewGenMapManager[ID comparable, ST any](fn func(ID) *ST) *MapManager[ID, ST] {
+	return &MapManager[ID, ST]{ss: i.NewMap[ID, *ST](), newFn: fn}
 }
 
-func GetGenMapFromContext[ID comparable, T any](ctx context.Context) *safemapManager[ID, T] {
-	return ctx.Value(mapManagerID{}).(*safemapManager[ID, T])
+func GetGenMapFromContext[ID comparable, T any](ctx context.Context) *MapManager[ID, T] {
+	return ctx.Value(mapManagerID{}).(*MapManager[ID, T])
 }
 
-func (sm *safemapManager[ID, ST]) Attach(ctx context.Context) context.Context {
+func (sm *MapManager[ID, ST]) Attach(ctx context.Context) context.Context {
 	return context.WithValue(ctx, mapManagerID{}, sm)
 }
 
-func (sm *safemapManager[ID, ST]) Get(id ID) (*ST, bool) {
+func (sm *MapManager[ID, ST]) Get(id ID) (*ST, bool) {
 	return sm.ss.Get(id)
 }
 
-func (sm *safemapManager[ID, ST]) Del(id ID) {
+func (sm *MapManager[ID, ST]) Del(id ID) {
 	sm.ss.Del(id)
 }
 
-func (sm *safemapManager[ID, ST]) Len(id ID) int {
+func (sm *MapManager[ID, ST]) Len(id ID) int {
 	return sm.ss.Len()
 }
 
-func (sm *safemapManager[ID, ST]) New(id ID) *ST {
+func (sm *MapManager[ID, ST]) New(id ID) *ST {
 	sess := sm.newFn(id)
 	sm.ss.Set(id, sess)
 	return sess
 }
 
-func (sm *safemapManager[ID, ST]) Clear() {
+func (sm *MapManager[ID, ST]) Clear() {
 	sm.ss = i.NewMap[ID, *ST]()
 }
 
-func (sm *safemapManager[ID, ST]) Apply(fn func(id ID, sess *ST)) {
+func (sm *MapManager[ID, ST]) Apply(fn func(id ID, sess *ST)) {
 	sm.ss.Mu.Lock()
 	defer sm.ss.Mu.Unlock()
 	for id, s := range sm.ss.Data {
@@ -55,7 +55,7 @@ func (sm *safemapManager[ID, ST]) Apply(fn func(id ID, sess *ST)) {
 	}
 }
 
-func (sm *safemapManager[ID, ST]) ForEach(fn func(id ID, sess ST)) {
+func (sm *MapManager[ID, ST]) ForEach(fn func(id ID, sess ST)) {
 	sm.ss.Mu.RLock()
 	defer sm.ss.Mu.RUnlock()
 	for id, s := range sm.ss.Data {
